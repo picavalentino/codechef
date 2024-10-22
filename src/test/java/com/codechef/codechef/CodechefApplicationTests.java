ef064205a18d576a3ca722ff4f18149269c0a79e
pica84 <pica84@nate.com>
2024-10-22 13:03:13 +0900

    아무런 설정이 되어있지 않음 초기 깡통파일입니다. 디펜던시는 Lombok, Spring Web Web, Thymeleaf, Spring Security, Spring Data JPA SQL, Validation I/O, PostgreSQL Driver SQL 적용해놓았습니다. 데이터베이스 설정 등은 추후 하도록 하겠습니다.

---
+++ src/test/java/com/codechef/codechef/CodechefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechef.codechef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
