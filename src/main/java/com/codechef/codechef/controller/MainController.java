24e9dc8d41b948065c5b0e317a21fd058a6f84b4
leeduhyeon <leeduhyeon12@gmail.com>
2024-10-28 12:59:45 +0900

    reservation_css_Basic_functions2

--- src/main/java/com/codechef/codechef/controller/MainController.java
+++ src/main/java/com/codechef/codechef/controller/MainController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -28,6 +29,69 @@ public class MainController {
     @GetMapping("/main")
     public String main() {
         return "/codechef/main";
+    }
+
+    // 검색 페이지
+    @GetMapping("/search")
+    public String search() {
+        return "/codechef/search";
+    }
+
+    // 상세 페이지
+    @GetMapping("/detail")
+    public String detail() {
+        return "/codechef/detail";
+    }
+
+    // 리뷰 작성 페이지
+    @GetMapping("/review-create")
+    public String reviewCreate() {
+        return "/codechef/reviewCreate";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/codechef/login";
+    }
+
+    @GetMapping("/insert")
+    public String insert() {
+        return "/codechef/join";
+    }
+
+    @PostMapping("/insert")
+    public String insertMember() {
+        return "/codechef/main";
+    }
+
+    // 방문예약 리스트 페이지
+    @GetMapping("/visit-expected")
+    public String visitExpected() {
+        return "/codechef/visit_expected";
+    }
+
+    // 방문완료 리스트 페이지
+    @GetMapping("/visit-completion")
+    public String visitCompletion() {
+        return "/codechef/visit_completion";
+    }
+
+    //마이페이지
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "/codechef/mypage";
+    }
+
+    //리뷰 보기 페이지
+    @GetMapping("/reviewView")
+    public String reviewView() {
+        return "/codechef/reviewView";
+    }
+
+    // 프로필 수정 페이지
+    @GetMapping("/profileEdit")
+    public String profileEdit(){
+        return "/codechef/profileEdit";
     }
 
     // 예약 페이지
